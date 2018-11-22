8737c19ffe032fbf8cdb82540e9badf857d83631
Medetbek.Mominov <medet.mominov@timelysoft.net>
2018-11-22 14:57:11 +0600

    implement header

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $( "#region-btn" ).on( "click", function() {
+        $( "#region-popup" ).toggleClass( "show" );
+        $( "#region-btn" ).toggleClass( "show" );
+    });
+});
