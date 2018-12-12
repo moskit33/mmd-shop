58b9bf818a83859cd0aed61d372c38106b5a453b
Medetbek.Mominov <medet.mominov@timelysoft.net>
2018-12-12 20:12:33 +0600

    temp

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,42 @@ $(document).ready(function() {
         $( "#region-popup" ).toggleClass( "show" );
         $( "#region-btn" ).toggleClass( "show" );
     });
+
+    $('.main-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        arrows: false,
+        appendDots:$(".slider-dots-wrap"),
+    });
+    
+    //Slideout-menu
+    var slideout = new Slideout({
+        'panel': document.getElementById('main'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70
+    });
+    
+    document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+        slideout.open();
+    });
+    
+    function close(eve) {
+        eve.preventDefault();
+        slideout.close();
+    }
+    
+    slideout
+    .on('beforeopen', function() {
+        this.panel.classList.add('panel-open');
+    })
+    .on('open', function() {
+        this.panel.addEventListener('click', close);
+    })
+    .on('beforeclose', function() {
+        this.panel.classList.remove('panel-open');
+        this.panel.removeEventListener('click', close);
+    });
+    //
 });
