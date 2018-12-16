2642723a2a1e892b18b5d9c444a1068cfef5ee46
Medetbek.Mominov <medet.mominov@timelysoft.net>
2018-12-17 03:20:22 +0600

    add sidebar and adaptive for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -11,34 +11,12 @@ $(document).ready(function() {
         arrows: false,
         appendDots:$(".slider-dots-wrap"),
     });
-    
-    //Slideout-menu
-    var slideout = new Slideout({
-        'panel': document.getElementById('main'),
-        'menu': document.getElementById('menu'),
-        'padding': 256,
-        'tolerance': 70
-    });
-    
-    document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
-        slideout.open();
-    });
-    
-    function close(eve) {
-        eve.preventDefault();
-        slideout.close();
-    }
-    
-    slideout
-    .on('beforeopen', function() {
-        this.panel.classList.add('panel-open');
-    })
-    .on('open', function() {
-        this.panel.addEventListener('click', close);
-    })
-    .on('beforeclose', function() {
-        this.panel.classList.remove('panel-open');
-        this.panel.removeEventListener('click', close);
-    });
     //
+    $( "#sidebar" ).simplerSidebar( {
+        align: "left",
+        selectors: {
+            trigger: ".toggle-sidebar",
+            quitter: ".close-sidebar"
+        }
+    } );
 });
