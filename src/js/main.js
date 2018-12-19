501bab52b3fa8dff76024b306693157d177b4bf6
Medetbek.Mominov <medet.mominov@timelysoft.net>
2018-12-19 16:04:51 +0600

    implement adaptive design

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
         appendDots:$(".slider-dots-wrap"),
     });
     //
+
+
     $( "#sidebar" ).simplerSidebar( {
         align: "left",
         selectors: {
@@ -19,4 +21,28 @@ $(document).ready(function() {
             quitter: ".close-sidebar"
         }
     } );
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (window.matchMedia("(min-width: 1400px)").matches) {
+        if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+        } else {
+        document.getElementById("header").style.top = "-230px";
+        }
+    } else if (window.matchMedia("(min-width: 576px)").matches) {
+        if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+        } else {
+        document.getElementById("header").style.top = "-128px";
+        }
+    } else {
+        if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+        } else {
+        document.getElementById("header").style.top = "-95px";
+        }
+    }
+    prevScrollpos = currentScrollPos;
+    }
 });
